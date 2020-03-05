f17985e0f33516ca361ba55042ddcb1ccb5ea014
miott <miott@cisco.com>
2020-03-05 08:22:23 -0800

    Added dependencies to make develop, checkpoint commit for gnmi

--- connector/src/yang/connector/gnmi.py
+++ connector/src/yang/connector/gnmi.py
@@ -43,7 +43,9 @@ class Gnmi(BaseConnection):
     os_class_map = {
         None: None,
         "iosxr": "IOS XR",
-        "nexus": "NX-OS",
+        # TODO: nxos cisco-gnmi support is not ready so use iosxe
+        "nxos": "NX-OS",
+        # "nxos": "IOS XE",
         "iosxe": "IOS XE",
     }
 
@@ -55,20 +57,19 @@ class Gnmi(BaseConnection):
             # TODO: what now?
             return
         # Initialize ClientBuilder
-        import pdb; pdb.set_trace()
         self.client_os = self.os_class_map.get(device.os, None)
         # ClientBuilder target is IP:Port
         target = dev_args.get('host') + ':' + str(dev_args.get('port'))
         builder = ClientBuilder(target).set_os(self.client_os)
         # Gather certificate settings
-        root = dev_args.get('root_certificate', '')
-        if os.path.isfile(root):
+        root = dev_args.get('root_certificate', None)
+        if root and os.path.isfile(root):
             root = open(root, 'rb').read()
-        chain = dev_args.get('certificate_chain', '')
-        if os.path.isfile(chain):
+        chain = dev_args.get('certificate_chain', None)
+        if chain and os.path.isfile(chain):
             chain = open(chain, 'rb').read()
-        private_key = dev_args.get('private_key', '')
-        if os.path.isfile(private_key):
+        private_key = dev_args.get('private_key', None)
+        if private_key and os.path.isfile(private_key):
             private_key= open(private_key, 'rb').read()
         builder.set_secure(root, private_key, chain)
         builder.set_ssl_target_override(
@@ -80,8 +81,8 @@ class Gnmi(BaseConnection):
         )
         # builder.construct() returns client and connects the channel
         self.builder = builder
-        log.info('GNMI builder ready')
-        self.gnmi = None
+        self.gnmi = self.builder.construct()
+        log.info(banner('gNMI READY'))
 
     @property
     def connected(self):
@@ -96,7 +97,13 @@ class Gnmi(BaseConnection):
             if prefix:
                 prefix = '/' + prefix
             return prefix
-        if not prefix:
+        for x in xpaths:
+            if not prefix:
+                prefix = x.keys()
+                continue
+            if prefix == x.keys():
+                continue
+            prefix = xpath_tokenizer_re.findall(x.keys())
             prefix = xpath_tokenizer_re.findall(xpaths[0])
             return self.get_prefix(xpaths[1:], prefix)
         else:
@@ -116,93 +123,63 @@ class Gnmi(BaseConnection):
             # Prefix matched so move on to next Xpath
             return self.get_prefix(xpaths[1:], prefix)
 
-    def xpath_to_path_elem(self, request):
-        paths = []
-        config = {
-            'update': [],
-            'replace': [],
-            'delete': []
-        }
-        if 'nodes' not in request:
-            # TODO: raw rpc?
-            return paths
-        else:
-            namespace_modules = {}
-            for prefix, nspace in request.get('namespace', {}).items():
-                module = ''
-                if '/Cisco-IOS-' in nspace:
-                    module = nspace[nspace.rfind('/') + 1:]
-                elif '/openconfig.net' in nspace:
-                    module = 'openconfig-'
-                    module += nspace[nspace.rfind('/') + 1:]
-                elif 'urn:ietf:params:xml:ns:yang:' in nspace:
-                    module = nspace.replace(
-                        'urn:ietf:params:xml:ns:yang:', '')
-                if module:
-                    namespace_modules[prefix] = module
-            for node in request.get('nodes', []):
-                if 'xpath' not in node:
-                    log.error('Xpath is not in message')
-                else:
-                    xpath = node['xpath']
-                    value = node.get('value', '')
-                    edit_op = node.get('edit-op', '')
-
-                    for pfx, ns in namespace_modules.items():
-                        xpath = xpath.replace(pfx + ':', ns + ':')
-                        value = value.replace(pfx + ':', ns + ':')
-                    if value or edit_op:
-                        if edit_op in ['create', 'merge', 'replace']:
-                            xpath_lst = xpath.split('/')
-                            name = xpath_lst.pop()
-                            xpath = '/'.join(xpath_lst)
-                            if edit_op == 'replace':
-                                if not config['replace']:
-                                    config['replace'] = [{
-                                        xpath: {name: value}
-                                    }]
-                                else:
-                                    config['replace'].append(
-                                        {xpath: {name: value}}
-                                    )
-                            else:
-                                if not config['update']:
-                                    config['update'] = [{
-                                        xpath: {name: value}
-                                    }]
-                                else:
-                                    config['update'].append(
-                                        {xpath: {name: value}}
-                                    )
-                        elif edit_op in ['delete', 'remove']:
-                            if config['delete']:
-                                config['delete'].add(xpath)
-                            else:
-                                config['delete'] = set(xpath)
-                    else:
-                        paths.append(
-                            self.gnmi.parse_xpath_to_gnmi_path(xpath)
-                        )
-
-        return paths, namespace_modules, config
-
-    def path_elem_to_xpath(self, path_elem, namespace):
+    def path_elem_to_xpath(self, path_elem, namespace={}, opfields=[]):
         """Convert a Path structure to an Xpath."""
         elems = path_elem.get('elem', [])
         xpath = []
+        keys = []
         for elem in elems:
             name = elem.get('name', '')
             if name:
                 for mod in namespace.values():
                     name = name.replace(mod + ':', '')
                 xpath.append(name)
+            key = elem.get('key', '')
+            if key:
+                for name, value in key.items():
+                    for mod in namespace.values():
+                        value = str(value).replace(mod + ':', '')
+                    opfields.append({
+                        'xpath': '/' + '/'.join(xpath) + '/' + name,
+                        'value': value
+                    })
         return '/' + '/'.join(xpath)
+
+    def get_opfields(self, val, xpath_str, opfields=[]):
+        if isinstance(val, dict):
+            for name, dict_val in val.items():
+                opfields = self.get_opfields(
+                    dict_val,
+                    xpath_str + '/' + name,
+                    opfields
+                )
+            return opfields
+        elif isinstance(val, list):
+            for item in val:
+                for name, dict_val in item.items():
+                    opfields = self.get_opfields(
+                        dict_val,
+                        xpath_str + '/' + name,
+                        opfields
+                    )
+            return opfields
+        else:
+            xpath_list = xpath_str.split('/')
+            name = xpath_list.pop()
+            xpath_str = '/'.join(xpath_list)
+            # MessageToDict does not decode all values as strings
+            opfields.append(
+                {'xpath': xpath_str + '/' + name,
+                 'value': str(val)}
+            )
+            return opfields
 
     def decode_response(self, response, namespace):
         """Decode a response from the google.protobuf into a dict."""
         resp_dict = json_format.MessageToDict(response)
         notifies = resp_dict.get('notification', [])
         ret_vals = []
+        opfields = []
         for notify in notifies:
             time_stamp = notify.get('timestamp', '')
             # TODO: convert time_stamp from str nanoseconds since epoch time
@@ -213,23 +190,29 @@ class Gnmi(BaseConnection):
                 opfields = []
                 xpath_str = self.path_elem_to_xpath(
                     update.get('path', {}),
-                    namespace
+                    namespace,
+                    opfields
                 )
                 if not xpath_str:
                     log.error('Xpath not determined from response')
                     continue
+                # TODO: the val depends on the encoding type
                 val = update.get('val', {}).get('jsonIetfVal', '')
+                if not val:
+                    val = update.get('val', {}).get('jsonVal', '')
                 if not val:
                     log.error('{0} has no values'.format(xpath_str))
                     continue
                 json_val = base64.b64decode(val).decode('utf-8')
-                val_dict = json.loads(json_val)
-                # MessageToDict does not decode all values as strings
-                for name, value in val_dict.items():
-                    opfields.append({
-                        'xpath': xpath_str + '/' + name,
-                        'value': str(value)
-                    })
+                update_val = json.loads(json_val)
+                if isinstance(update_val, dict):
+                    update_val = [update_val]
+                for val_dict in update_val:
+                    opfields = self.get_opfields(
+                        val_dict,
+                        xpath_str,
+                        opfields
+                    )
                 ret_val['update'] = opfields
             deletes = notify.get('delete', [])
             deleted = []
@@ -245,6 +228,25 @@ class Gnmi(BaseConnection):
             ret_vals.append(ret_val)
         return ret_vals
 
+    def decode_capabilities(self, caps={}):
+        return_caps = {
+            'version': 'unknown',
+            'encodings': 'unknown',
+            'models': []
+        }
+        if not caps:
+            return {}
+        caps_dict = json_format.MessageToDict(caps)
+        for key, value in caps_dict.items():
+            if key == 'gNMIVersion':
+                return_caps['version'] = value
+            elif key == 'supportedEncodings':
+                return_caps['encodings'] = value
+            elif key == 'supportedModels':
+                return_caps['models'] = value
+
+        return return_caps
+
     def set(self, cmd):
         """Send any Set data command.
 
@@ -257,17 +259,17 @@ class Gnmi(BaseConnection):
             self.connect()
         try:
             # Convert xpath to path element
-            import pdb; pdb.set_trace()
             responses = []
-            paths, ns, configs = self.xpath_to_path_elem(cmd)
+            ns, configs = self.gnmi.xpath_to_path_elem(cmd)
             updates = configs.get('update')
             replaces = configs.get('replace')
             deletes = configs.get('delete')
             if updates or replaces:
-                if hasattr(self.gnmi, 'set_json'):
-                    responses.append(self.gnmi.set_json(
+                responses.append(
+                    self.gnmi.set_json(
                         updates,
                         replaces,
+                        ietf=False
                     ))
             if deletes:
                 responses.append(self.gnmi.delete_xpaths(deletes))
@@ -279,21 +281,24 @@ class Gnmi(BaseConnection):
     def configure(self, cmd):
         return self.set(cmd)
 
-    def get(self, cmd):
+    def get(self, cmd, datatype='STATE'):
         """Send any Get data commmand.
 
         Args:
           cmd (str): Configuration CLI command.
+          datatype (str): [ ALL | STATE ] (default: STATE)
         Returns:
           (str): CLI response
         """
         if not self.connected:
             self.connect()
         try:
-            import pdb; pdb.set_trace()
             # Convert xpath to path element
-            request, ns, configs = self.xpath_to_path_elem(cmd)
-            resp = self.gnmi.get(request)
+            ns, msg = self.gnmi.xpath_to_path_elem(cmd)
+            resp = self.gnmi.get_xpaths(
+                msg.get('get', []),
+                datatype
+            )
             # Do fixup on response
             response = self.decode_response(resp, ns)
             return response
@@ -301,18 +306,29 @@ class Gnmi(BaseConnection):
             log.error(banner('{0}: {1}'.format(exe.code(), exe.details())))
             return []
 
+    def get_config(self, cmd):
+        """Send any get-config data commmand.
+
+        Args:
+          cmd (str): Configuration CLI command.
+        Returns:
+          (str): CLI response
+        """
+        return self.get(cmd, datatype='ALL')
+
     def execute(self, cmd):
         return self.get(cmd)
 
-
-    def capabilities(self, request):
+    def capabilities(self):
         """Retrieve capabilities from device."""
         if not self.connected:
             self.connect()
         try:
-            return self.gnmi.capabilities()
+            response = json_format.MessageToDict(self.gnmi.capabilities())
+            return response
         except Exception as exe:
             log.error(banner('{0}: {1}'.format(exe.code(), exe.details())))
+            return {}
 
     def subscribe(self, cmd):
         """Subscribe to notification stream."""
@@ -320,12 +336,17 @@ class Gnmi(BaseConnection):
             self.connect()
         try:
             # Convert xpath to path element
-            request = self.xpath_to_path_elem(cmd)
+            import pdb; pdb.set_trace()
+            request = self.gnmi.xpath_to_path_elem(cmd)
             resp = self.gnmi.subscribe(request)
             # Do fixup in response
             return resp
         except Exception as exe:
             log.error(banner('{0}: {1}'.format(exe.code(), exe.details())))
+
+    def get_updates(self, response):
+        """Notification check."""
+        return self.decode_response(response)
 
     def connect(self):
         """Connect to gRPC device."""
@@ -337,6 +358,7 @@ class Gnmi(BaseConnection):
         #   authority
         if not self.gnmi:
             self.gnmi = self.builder.construct()
+            log.info(banner('gNMI CONNECTED'))
 
     def disconnect(self):
         """Disconnect from SSH device."""
