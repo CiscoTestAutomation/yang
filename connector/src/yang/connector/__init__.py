21ed7d22e48dfe317b35170653cb94512200c6ce
lgerrior <lgerrior@cisco.com>
2023-01-25 12:25:56 -0500

    Open branches for release 23.1

--- connector/src/yang/connector/__init__.py
+++ connector/src/yang/connector/__init__.py
@@ -7,7 +7,7 @@ Restconf implementation is coming next.
 """
 
 # metadata
-__version__ = '22.11.1b0'
+__version__ = '23.1'
 __author__ = (
     'Jonathan Yang <yuekyang@cisco.com>',
     'Siming Yuan <siyuan@cisco.com',
