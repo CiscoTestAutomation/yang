2b83e0e190f188fb0f2b50c7a8cccf8c291974c0
miott <miott@cisco.com>
2023-05-08 15:07:32 -0700

    Update settings.py

--- connector/src/yang/connector/settings.py
+++ connector/src/yang/connector/settings.py
@@ -8,8 +8,3 @@ class Settings(dict):
         self.NETCONF_SCREEN_LOGGING_MAX_LINES = 40
         # Enable XML formatting by default
         self.NETCONF_LOGGING_FORMAT_XML = True
-
-    def __repr__(self):
-        return '%s(%s)' % (self.__class__.__name__,
-                        super(AttrDict, self).__repr__())
-
