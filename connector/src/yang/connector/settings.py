5955dff44fb83ced43783d8c99d0666ae3f380d2
miott <miott@cisco.com>
2023-05-08 14:03:36 -0700

    Address review comment.

--- connector/src/yang/connector/settings.py
+++ connector/src/yang/connector/settings.py
@@ -1,14 +1,15 @@
-try:
-    from pyats.datastructures import AttrDict
-except ImportError:
-    class AttrDict:
-        pass
 
+class Settings(dict):
 
-class Settings(AttrDict):
-
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__dict__ = self
         # Default number of lines to log to screen
         self.NETCONF_SCREEN_LOGGING_MAX_LINES = 40
         # Enable XML formatting by default
         self.NETCONF_LOGGING_FORMAT_XML = True
+
+    def __repr__(self):
+        return '%s(%s)' % (self.__class__.__name__,
+                        super(AttrDict, self).__repr__())
+
