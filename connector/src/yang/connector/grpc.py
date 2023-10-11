9e53988934a9ab326262293328a9e13940b3b74c
GerriorL <84335026+gerriorl@users.noreply.github.com>
2023-10-11 14:21:17 -0400

    Commit suggestions

--- connector/src/yang/connector/grpc.py
+++ connector/src/yang/connector/grpc.py
@@ -2,36 +2,15 @@ import logging
 import socket
 import configparser
 import subprocess
+from shutil import copyfile
 
-try:
-    from pyats.log.utils import banner
-    from pyats.connections import BaseConnection
-    from pyats.utils.secret_strings import to_plaintext
-    from pyats.easypy import runtime
-    from genie.libs.sdk.apis.utils import get_local_ip
-    from genie.libs.sdk.apis.iosxe.telemetry.configure import *
-    from unicon import Connection
-except ImportError:
-    # Standalone without pyats install
-    class BaseConnection:
-        class dev:
-            def __init__(self, dev_os):
-                self.os = dev_os
-
-        def __init__(self, device_os, **kwargs):
-            self.connection_info = {'protocol': 'gnmi'}
-            self._device = self.dev(device_os)
-            self.connection_info.update(kwargs)
-
-    class runtime:
-        import os
-        directory = os.curdir
-
-    def banner(string):
-        return string
-
-    def to_plaintext(string):
-        return string
+from pyats.connections import BaseConnection
+from pyats.easypy import runtime
+from pyats.utils.secret_strings import to_plaintext
+from pyats.easypy import runtime
+from genie.libs.sdk.apis.utils import get_local_ip
+from genie.libs.sdk.apis.iosxe.telemetry.configure import *
+from unicon import Connection
 
 
 # create a logger for this module
@@ -44,6 +23,11 @@ class Grpc(BaseConnection):
         Can be used with pyATS same as yang.connector.Netconf is used or
         can be used as a standalone module.
     """
+    def __new__(cls, *args, **kwargs):
+        transporter = kwargs.get('transporter', 'telegraf')
+        if transporter.lower() == 'telegraf':
+            return super().__new__(GrpcTelegraf)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.device = kwargs.get('device')
@@ -71,86 +55,99 @@ class Grpc(BaseConnection):
         self.host = dev_args.get('host') or dev_args.get('ip')
 
         self.transporter = dev_args.get('transporter', 'telegraf').lower()
-        self.output_file = dev_args.get('output_file', f'{runtime.directory}/mdt')
-        self.config_file = dev_args.get('config_file', None)
+        try:
+            self.output_file = copyfile(dev_args.get('output_file', None), f"{runtime.directory}/mdt")
+        except TypeError:
+            self.output_file = f"{runtime.directory}/mdt"
+        try:
+            self.config_file = copyfile(dev_args.get('config_file', None), f"{runtime.directory}/telegraf.conf")
+        except TypeError:
+            self.config_file = None
         self.telemetry_subscription_id = dev_args.get('telemetry_subscription_id', 11172017)
 
         self.transport_process = None
 
+    def connect(self):
+        raise NotImplementedError
+
+    def disconnect(self):
+        self.transport_process.terminate()
+        self.device.api.unconfigure_telemetry_ietf_subscription(self.telemetry_subscription_id)
+        self.device.disconnect()
+
+
+class GrpcTelegraf(Grpc):
     @property
     def connected(self):
         """Return True if session is connected."""
         return True
 
     def connect(self):
-        allocated_port = None
-
         # Allocate a random available port to localhost
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as grpc_socket:
             grpc_socket.bind(('localhost', 0))
             _, allocated_port = grpc_socket.getsockname()
 
             # run config generation within context manager to hold port until it can be passed to telegraf
-            if self.transporter == 'telegraf':
-                config = configparser.ConfigParser()
-                if self.config_file:
-                    # load configuration file
-                    config.read(self.config_file)
+            config = configparser.ConfigParser()
+            if self.config_file:
+                # load configuration file
+                config.read(self.config_file)
+                if '[inputs.cisco_telemetry_mdt]' not in config.sections():
+                    config.add_section('[inputs.cisco_telemetry_mdt]')
+
+                # update input socket listener
+                config.set('[inputs.cisco_telemetry_mdt]', 'transport', '"grpc"')
+                config.set('[inputs.cisco_telemetry_mdt]', 'service_address', f'":{allocated_port}"')
+
+                # write configuration file
+                with open(f"runtime.directory", 'w') as f:
+                    log.info(f"Writing config to {self.config_file}")
+                    config.write(f)
+            else:
+                # set config file path
+                self.config_file = f"{runtime.directory}/telegraf.conf"
+                config.read(self.config_file)
+
+                # if the file already exists, only update the port
+                if config.sections():
                     if '[inputs.cisco_telemetry_mdt]' not in config.sections():
                         config.add_section('[inputs.cisco_telemetry_mdt]')
 
-                    # update input socket listener
                     config.set('[inputs.cisco_telemetry_mdt]', 'transport', '"grpc"')
                     config.set('[inputs.cisco_telemetry_mdt]', 'service_address', f'":{allocated_port}"')
 
-                    # write configuration file
                     with open(self.config_file, 'w') as f:
                         log.info(f"Writing config to {self.config_file}")
                         config.write(f)
                 else:
-                    # set config file path
-                    self.config_file = f"{runtime.directory}/telegraf.conf"
-                    config.read(self.config_file)
+                    # generate a default configuration file
+                    with open(self.config_file, 'w') as f:
+                        log.info(f"Creating telegraf config file {self.config_file}")
+                        f.write('')
 
-                    # if the file already exists, only update the port
-                    if config.sections():
-                        if '[inputs.cisco_telemetry_mdt]' not in config.sections():
-                            config.add_section('[inputs.cisco_telemetry_mdt]')
+                    # create default config
+                    # global tags
+                    config.add_section('global_tags')
+                    config.set('global_tags', 'user', r'"${USER}"')
 
-                        config.set('[inputs.cisco_telemetry_mdt]', 'transport', '"grpc"')
-                        config.set('[inputs.cisco_telemetry_mdt]', 'service_address', f'":{allocated_port}"')
+                    # input configuration
+                    config.add_section('[inputs.cisco_telemetry_mdt]')
+                    config.set('[inputs.cisco_telemetry_mdt]', 'transport', '"grpc"')
+                    config.set('[inputs.cisco_telemetry_mdt]', 'service_address', f'":{allocated_port}"')
 
-                        with open(self.config_file, 'w') as f:
-                            log.info(f"Writing config to {self.config_file}")
-                            config.write(f)
-                    else:
-                        # generate a default configuration file
-                        with open(self.config_file, 'w') as f:
-                            log.info(f"Creating telegraf config file {self.config_file}")
-                            f.write('')
+                    # default output config - to file in runtime or user supplied dir
+                    config.add_section('[outputs.file]')
+                    config.set('[outputs.file]', 'files', f'["stdout", "{self.output_file}"]')
+                    config.set('[outputs.file]', 'data_format', '"json"')
+                    config.set('[outputs.file]', 'json_timestamp_units', '"1ms"')
+                    config.set('[outputs.file]', 'rotation_max_size', '"2048MB"')
+                    config.set('[outputs.file]', 'flush_jitter', '"500ms"')
 
-                        # create default config
-                        # global tags
-                        config.add_section('global_tags')
-                        config.set('global_tags', 'user', r'"${USER}"')
-
-                        # input configuration
-                        config.add_section('[inputs.cisco_telemetry_mdt]')
-                        config.set('[inputs.cisco_telemetry_mdt]', 'transport', '"grpc"')
-                        config.set('[inputs.cisco_telemetry_mdt]', 'service_address', f'":{allocated_port}"')
-
-                        # default output config - to file in runtime or user supplied dir
-                        config.add_section('[outputs.file]')
-                        config.set('[outputs.file]', 'files', f'["stdout", "{self.output_file}"]')
-                        config.set('[outputs.file]', 'data_format', '"json"')
-                        config.set('[outputs.file]', 'json_timestamp_units', '"1ms"')
-                        config.set('[outputs.file]', 'rotation_max_size', '"2048MB"')
-                        config.set('[outputs.file]', 'flush_jitter', '"500ms"')
-
-                        # apply config
-                        with open(self.config_file, 'w') as f:
-                            log.info(f"Updating {self.config_file}")
-                            config.write(f)
+                    # apply config
+                    with open(self.config_file, 'w') as f:
+                        log.info(f"Updating {self.config_file}")
+                        config.write(f)
 
         # exit context manager to release port
         # spawn telegraf/pipeline using config
@@ -160,13 +157,12 @@ class Grpc(BaseConnection):
         log.info(f"Telegraf is running as PID {self.transport_process.pid} on port {allocated_port}")
 
         # call the API to genie configure the service on the device
-        self.device.connect()
+        self.device.instantiate()
+        if self.device.default.__class__:
+            self.device.connect()
+        else:
+            raise ValueError('Connection Class is not Unicon')
         local_ip = self.device.api.get_local_ip()
         self.device.api.configure_telemetry_ietf_parameters(self.telemetry_subscription_id,
                                                             "yang-push", local_ip, allocated_port, "grpc-tcp")
         log.info(f"Started gRPC inbound server on {local_ip}:{allocated_port}")
-
-    def disconnect(self):
-        self.transport_process.terminate()
-        self.device.api.unconfigure_telemetry_ietf_subscription(self.telemetry_subscription_id)
-        self.device.disconnect()
