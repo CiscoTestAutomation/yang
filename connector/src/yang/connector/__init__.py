41632bc78259ae6ba06923ad044aa91c3978cd80
Ben Astell (bastell) <bastell@cisco.com>
2024-05-23 18:09:40 -0400

    Open branches for release_24.5

--- connector/src/yang/connector/__init__.py
+++ connector/src/yang/connector/__init__.py
@@ -7,7 +7,7 @@ Restconf implementation is coming next.
 """
 
 # metadata
-__version__ = '24.4'
+__version__ = '24.5'
 __author__ = (
     'Jonathan Yang <yuekyang@cisco.com>',
     'Siming Yuan <siyuan@cisco.com',
