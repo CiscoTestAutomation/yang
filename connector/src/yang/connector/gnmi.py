9f730e2eaf4854ad0ea21b8193d32d54e03faeb2
Parimi Bala Neeraja <paneeraj@cisco.com>
2024-11-07 18:22:53 +0530

    Updated with get

--- connector/src/yang/connector/gnmi.py
+++ connector/src/yang/connector/gnmi.py
@@ -308,8 +308,8 @@ class Gnmi(BaseConnection):
             port = str(dev_args.get('port'))
         target = '{0}:{1}'.format(host, port)
 
-        max_receive_message_length = self.settings.GRPC_MAX_RECEIVE_MESSAGE_LENGTH
-        max_send_message_length = self.settings.GRPC_MAX_SEND_MESSAGE_LENGTH
+        max_receive_message_length = self.settings.get('GRPC_MAX_RECEIVE_MESSAGE_LENGTH')
+        max_send_message_length = self.settings.get('GRPC_MAX_SEND_MESSAGE_LENGTH')
         
         options = [('grpc.max_receive_message_length', max_receive_message_length),
                    ('grpc.max_send_message_length', max_send_message_length)]
