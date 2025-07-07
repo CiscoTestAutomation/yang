cf7137bd125e9677e66b7434ac7b0c74586d3408
Thomas Ryan <thomryan@cisco.com>
2025-07-07 15:03:50 -0400

    Open branches for release_25.6

--- connector/src/yang/connector/__init__.py
+++ connector/src/yang/connector/__init__.py
@@ -7,7 +7,7 @@ Restconf implementation is coming next.
 """
 
 # metadata
-__version__ = "25.5"
+__version__ = "25.6"
 __author__ = (
     'Jonathan Yang <yuekyang@cisco.com>',
     'Siming Yuan <siyuan@cisco.com',
