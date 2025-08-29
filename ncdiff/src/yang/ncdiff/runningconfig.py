1bee06ac2250d3ff463eec6cf9488a00940618cb
Sowmya D N <sodn@cisco.com>
2025-08-29 12:51:01 +0530

    Orderless configuration

--- ncdiff/src/yang/ncdiff/runningconfig.py
+++ ncdiff/src/yang/ncdiff/runningconfig.py
@@ -108,6 +108,7 @@ ORDERLESS_COMMANDS = [
     (re.compile(r'^ *member vni '), 1),
     (re.compile(r'^ *ipv6 route '), 0),
     (re.compile(r'^ *ip route '), 0),
+    (re.compile(r'^ *ip wccp '), 1),
 ]
 
 # Some commands can be overwritten without a no command. For example, changing
