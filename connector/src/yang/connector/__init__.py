930367a53649f6271d0ca999e105dc59e2630818
Lukeman Hakkim Sheik Alavudeen <lsheikal@cisco.com>
2026-02-27 14:03:31 -0500

    Open branches for release_26.2

--- connector/src/yang/connector/__init__.py
+++ connector/src/yang/connector/__init__.py
@@ -7,7 +7,7 @@ Restconf implementation is coming next.
 """
 
 # metadata
-__version__ = "26.1"
+__version__ = "26.2"
 __author__ = (
     'Jonathan Yang <yuekyang@cisco.com>',
     'Siming Yuan <siyuan@cisco.com',
